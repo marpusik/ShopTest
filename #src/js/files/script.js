1477c44f6ee3f6f6b0f4c158e276f15daf0621a2
marpusik <marpusik1987@gmail.com>
2021-08-19 22:00:59 +0300

    plus9

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -17,4 +17,14 @@ window.onload = function () {
             document.querySelector('.search-form').classList.remove('_active');
         }
     }
+    const headerElement = document.querySelector('.header');
+    const callback = function (entries, observer) {
+        if (entries[0].isIntersecting){
+            headerElement.classList.remove('_scroll');
+        } else {
+            headerElement.classList.add('_scroll');
+        }
+    };
+    const headerObserver = new IntersectionObserver(callback);
+    headerObserver.observe(headerElement);
 }
